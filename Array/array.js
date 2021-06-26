d452b37e0362018325308c14fedb1eb6423d4bfa
Anastasiya Kuznetcova <72085144+KuznetcovaAsya@users.noreply.github.com>
2021-06-26 12:20:58 +0700

    array

--- Array/array.js
+++ Array/array.js
@@ -1,7 +1,10 @@
 var array1 = [1, 1, 3, 8, 5, -6, 7, -8, -25, 17, 11, 35, 13, 14, 120, 17];
 console.log("Массив 1: " + array1);
 
-var sortedArray = array1.sort((a, b) => b - a);
+var sortedArray = array1.sort(function (a, b) {
+    return b - a;
+});
+
 console.log("Массив 1 отсортирован по убыванию: " + sortedArray);
 
 var arrayFromStartNumbers = array1.slice(0, 5);
@@ -10,8 +13,12 @@ console.log("Подмассив из первых пяти элементов М
 var arrayFromEndNumbers = array1.slice(-5);
 console.log("Подмассив из последних пяти элементов Массива 1: " + arrayFromEndNumbers);
 
-var evenNumbersSum = array1.filter(e => e % 2 === 0)
-    .reduce((sum, current) => sum + current);
+var evenNumbersSum = array1.filter(function (e) {
+    return e % 2 === 0;
+}).reduce(function (sum, current) {
+    return sum + current;
+});
+
 console.log("Cумма четных чисел Mассива 1: " + evenNumbersSum);
 
 var array2 = [];
@@ -21,5 +28,10 @@ for (var i = 0; i < 100; ++i) {
 
 console.log("Массив 2: " + array2);
 
-var numbersSquares = array2.filter(e => e % 2 === 0);
+var numbersSquares = array2.filter(function (e) {
+    return e % 2 === 0;
+}).map(function (e) {
+    return e * e;
+});
+
 console.log("Список квадратов четных чисел Массива 2: " + numbersSquares);
---
+++ Contries/countries.js
@@ -0,0 +1,79 @@
+var countries = [
+    {
+        countryName: "Россия",
+        cities: [
+            {
+                cityName: "Новосибирск",
+                population: 1.511,
+            },
+            {
+                cityName: "Москва",
+                population: 11.92,
+            },
+            {
+                cityName: "Красноярск",
+                population: 1.007,
+            },
+            {
+                cityName: "Санкт-Петербург",
+                population: 4.991
+            }
+        ]
+    },
+    {
+        countryName: "Германия",
+        cities: [
+            {
+                cityName: "Берлин",
+                population: 3.645,
+            },
+            {
+                cityName: "Мюнхен",
+                population: 1.472,
+            },
+            {
+                cityName: "Гамбург",
+                population: 1.841
+            }
+        ]
+    },
+    {
+        countryName: "Канада",
+        cities: [
+            {
+                cityName: "Торонто",
+                population: 2.93,
+            },
+            {
+                cityName: "Монреаль",
+                population: 1.78,
+            },
+            {
+                cityName: "Ванкувер",
+                population: 0.675
+            }
+        ]
+    },
+    {
+        countryName: "Япония",
+        cities: [
+            {
+                cityName: "Токио",
+                population: 14,
+            },
+            {
+                cityName: "Киото",
+                population: 1.475
+            }
+        ]
+    }
+]
+
+console.log(countries.filter());
+
+
+
+
+
+
+
