21e944784e94da57a7029e1cce88a1d0562113b6
Anastasiya Kuznetcova <72085144+KuznetcovaAsya@users.noreply.github.com>
2021-06-28 21:59:47 +0700

    array

--- Array/array.js
+++ Array/array.js
@@ -1,37 +1,51 @@
-var array1 = [1, 1, 3, 8, 5, -6, 7, -8, -25, 17, 11, 35, 13, 14, 120, 17];
-console.log("Массив 1: " + array1);
+(function () {
+    var array1 = [1, 1, 3, 8, 5, -6, 7, -8, -25, 17, 11, 35, 13, 14, 120, 17];
+    console.log("Массив 1: " + array1);
 
-var sortedArray = array1.sort(function (a, b) {
-    return b - a;
-});
+    console.log("Массив 1 отсортирован по убыванию: " + sortDescending(array1));
 
-console.log("Массив 1 отсортирован по убыванию: " + sortedArray);
+    console.log("Подмассив из первых пяти элементов Массива 1: " + getStartNumbers(array1));
 
-var arrayFromStartNumbers = array1.slice(0, 5);
-console.log("Подмассив из первых пяти элементов Массива 1: " + arrayFromStartNumbers);
+    console.log("Подмассив из последних пяти элементов Массива 1: " + getEndNumbers(array1));
 
-var arrayFromEndNumbers = array1.slice(-5);
-console.log("Подмассив из последних пяти элементов Массива 1: " + arrayFromEndNumbers);
+    console.log("Cумма четных чисел Mассива 1: " + getEvenNumbersSum(array1));
 
-var evenNumbersSum = array1.filter(function (e) {
-    return e % 2 === 0;
-}).reduce(function (sum, current) {
-    return sum + current;
-}, 0);
+    var array2 = [];
+    for (var i = 0; i < 100; ++i) {
+        array2.push(i + 1);
+    }
 
-console.log("Cумма четных чисел Mассива 1: " + evenNumbersSum);
+    console.log("Массив 2: " + array2);
 
-var array2 = [];
-for (var i = 0; i < 100; ++i) {
-    array2[i] = i + 1;
-}
+    console.log("Список квадратов четных чисел Массива 2: " + getEvenNumbersSquares());
 
-console.log("Массив 2: " + array2);
+    function sortDescending(array) {
+        return array1.sort(function (a, b) {
+            return b - a;
+        });
+    }
 
-var numbersSquares = array2.filter(function (e) {
-    return e % 2 === 0;
-}).map(function (e) {
-    return e * e;
-});
+    function getStartNumbers(array) {
+        return array1.slice(0, 5);
+    }
 
-console.log("Список квадратов четных чисел Массива 2: " + numbersSquares);
+    function getEndNumbers(array) {
+        return array1.slice(-5);
+    }
+
+    function getEvenNumbersSum(array) {
+        return array1.filter(function (e) {
+            return e % 2 === 0;
+        }).reduce(function (sum, current) {
+            return sum + current;
+        }, 0);
+    }
+
+    function getEvenNumbersSquares(array) {
+        return array2.filter(function (e) {
+            return e % 2 === 0;
+        }).map(function (e) {
+            return e * e;
+        });
+    }
+}());
--- Countries/countries.js
+++ Countries/countries.js
@@ -1,108 +1,110 @@
-var countries = [
-    {
-        countryName: "Россия",
-        cities: [
-            {
-                cityName: "Новосибирск",
-                population: 1.511,
-            },
-            {
-                cityName: "Москва",
-                population: 11.92,
-            },
-            {
-                cityName: "Красноярск",
-                population: 1.007,
-            },
-            {
-                cityName: "Санкт-Петербург",
-                population: 4.991
-            }
-        ]
-    },
-    {
-        countryName: "Германия",
-        cities: [
-            {
-                cityName: "Берлин",
-                population: 3.645,
-            },
-            {
-                cityName: "Мюнхен",
-                population: 1.472,
-            },
-            {
-                cityName: "Гамбург",
-                population: 1.841
-            }
-        ]
-    },
-    {
-        countryName: "Канада",
-        cities: [
-            {
-                cityName: "Торонто",
-                population: 2.93,
-            },
-            {
-                cityName: "Монреаль",
-                population: 1.78,
-            },
-            {
-                cityName: "Ванкувер",
-                population: 0.675
-            }
-        ]
-    },
-    {
-        countryName: "Япония",
-        cities: [
-            {
-                cityName: "Токио",
-                population: 14,
-            },
-            {
-                cityName: "Киото",
-                population: 1.475
-            }
-        ]
-    }
-]
-
-function getMaxCitiesCount(countries) {
-    var maxCitiesCount = 0;
-    var maxCitiesCountCountries = [];
-
-    countries.forEach(function (country) {
-        if (country.cities.length > maxCitiesCount) {
-            maxCitiesCount = country.cities.length;
-            maxCitiesCountCountries.push(country);
-        } else if (country.cities.length === maxCitiesCount) {
-            maxCitiesCountCountries.push(country);
+(function () {
+    var countries = [
+        {
+            countryName: "Россия",
+            cities: [
+                {
+                    cityName: "Новосибирск",
+                    population: 1.511,
+                },
+                {
+                    cityName: "Москва",
+                    population: 11.92,
+                },
+                {
+                    cityName: "Красноярск",
+                    population: 1.007,
+                },
+                {
+                    cityName: "Санкт-Петербург",
+                    population: 4.991
+                }
+            ]
+        },
+        {
+            countryName: "Германия",
+            cities: [
+                {
+                    cityName: "Берлин",
+                    population: 3.645,
+                },
+                {
+                    cityName: "Мюнхен",
+                    population: 1.472,
+                },
+                {
+                    cityName: "Гамбург",
+                    population: 1.841
+                }
+            ]
+        },
+        {
+            countryName: "Канада",
+            cities: [
+                {
+                    cityName: "Торонто",
+                    population: 2.93,
+                },
+                {
+                    cityName: "Монреаль",
+                    population: 1.78,
+                },
+                {
+                    cityName: "Ванкувер",
+                    population: 0.675
+                }
+            ]
+        },
+        {
+            countryName: "Япония",
+            cities: [
+                {
+                    cityName: "Токио",
+                    population: 14,
+                },
+                {
+                    cityName: "Киото",
+                    population: 1.475
+                }
+            ]
         }
-    });
+    ]
 
-    return maxCitiesCountCountries;
-}
+    function getMaxCitiesCount(countries) {
+        var maxCitiesCount = 0;
+        var maxCitiesCountCountries = [];
 
-console.log("Страны с максимальным количеством городов: ");
-console.log(getMaxCitiesCount(countries));
+        countries.forEach(function (country) {
+            if (country.cities.length > maxCitiesCount) {
+                maxCitiesCount = country.cities.length;
+                maxCitiesCountCountries.push(country);
+            } else if (country.cities.length === maxCitiesCount) {
+                maxCitiesCountCountries.push(country);
+            }
+        });
 
-function getCountriesPopulation(countries) {
-    var countriesPopulation = new Object();
-
-    countries.forEach(function (country) {
-        countriesPopulation[country.countryName] = getCountryPopulation(country);
-    });
-
-    function getCountryPopulation(country) {
-        return country.cities.reduce(function (population, city) {
-            return population + city.population;
-        }, 0);
+        return maxCitiesCountCountries;
     }
 
-    return countriesPopulation;
-}
+    console.log("Страны с максимальным количеством городов: ");
+    console.log(getMaxCitiesCount(countries));
 
-console.log("Страны и их население: ");
-console.log(getCountriesPopulation(countries));
+    function getCountriesPopulation(countries) {
+        var countriesPopulation = new Object();
+
+        countries.forEach(function (country) {
+            countriesPopulation[country.countryName] = getCountryPopulation(country);
+        });
+
+        function getCountryPopulation(country) {
+            return country.cities.reduce(function (population, city) {
+                return population + city.population;
+            }, 0);
+        }
+
+        return countriesPopulation;
+    }
+
+    console.log("Страны и их население: ");
+    console.log(getCountriesPopulation(countries));
+}());
