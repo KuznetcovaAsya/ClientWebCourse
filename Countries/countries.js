c1b4579eafb9cc6ee1aef2cdebeb8d114ac35fa0
Anastasiya Kuznetcova <72085144+KuznetcovaAsya@users.noreply.github.com>
2021-07-01 20:07:34 +0700

    countries

--- Countries/countries.js
+++ Countries/countries.js
@@ -1,83 +1,83 @@
 (function () {
     var countries = [
         {
-            countryName: "Россия",
+            name: "Германия",
             cities: [
                 {
-                    cityName: "Новосибирск",
-                    population: 1.511,
+                    name: "Берлин",
+                    population: 3.645
                 },
                 {
-                    cityName: "Москва",
-                    population: 11.92,
+                    name: "Мюнхен",
+                    population: 1.472
                 },
                 {
-                    cityName: "Красноярск",
-                    population: 1.007,
-                },
-                {
-                    cityName: "Санкт-Петербург",
-                    population: 4.991
-                }
-            ]
-        },
-        {
-            countryName: "Германия",
-            cities: [
-                {
-                    cityName: "Берлин",
-                    population: 3.645,
-                },
-                {
-                    cityName: "Мюнхен",
-                    population: 1.472,
-                },
-                {
-                    cityName: "Гамбург",
+                    name: "Гамбург",
                     population: 1.841
                 }
             ]
         },
         {
-            countryName: "Канада",
+            name: "Канада",
             cities: [
                 {
-                    cityName: "Торонто",
-                    population: 2.93,
+                    name: "Торонто",
+                    population: 2.93
                 },
                 {
-                    cityName: "Монреаль",
-                    population: 1.78,
+                    name: "Монреаль",
+                    population: 1.78
                 },
                 {
-                    cityName: "Ванкувер",
+                    name: "Ванкувер",
                     population: 0.675
                 }
             ]
         },
         {
-            countryName: "Япония",
+            name: "Япония",
             cities: [
                 {
-                    cityName: "Токио",
-                    population: 14,
+                    name: "Токио",
+                    population: 14
                 },
                 {
-                    cityName: "Киото",
+                    name: "Киото",
                     population: 1.475
                 }
             ]
+        },
+        {
+            name: "Россия",
+            cities: [
+                {
+                    name: "Новосибирск",
+                    population: 1.511
+                },
+                {
+                    name: "Москва",
+                    population: 11.92
+                },
+                {
+                    name: "Красноярск",
+                    population: 1.007
+                },
+                {
+                    name: "Санкт-Петербург",
+                    population: 4.991
+                }
+            ]
         }
-    ]
+    ];
 
-    function getMaxCitiesCount(countries) {
+    function getMaxCitiesCountCountries(countries) {
         var maxCitiesCount = 0;
         var maxCitiesCountCountries = [];
 
         countries.forEach(function (country) {
             if (country.cities.length > maxCitiesCount) {
                 maxCitiesCount = country.cities.length;
-                maxCitiesCountCountries.push(country);
+                maxCitiesCountCountries = [country];
             } else if (country.cities.length === maxCitiesCount) {
                 maxCitiesCountCountries.push(country);
             }
@@ -87,20 +87,20 @@
     }
 
     console.log("Страны с максимальным количеством городов: ");
-    console.log(getMaxCitiesCount(countries));
+    console.log(getMaxCitiesCountCountries(countries));
 
     function getCountriesPopulation(countries) {
         var countriesPopulation = new Object();
-
-        countries.forEach(function (country) {
-            countriesPopulation[country.countryName] = getCountryPopulation(country);
-        });
 
         function getCountryPopulation(country) {
             return country.cities.reduce(function (population, city) {
                 return population + city.population;
             }, 0);
         }
+
+        countries.forEach(function (country) {
+            countriesPopulation[country.name] = getCountryPopulation(country);
+        });
 
         return countriesPopulation;
     }
