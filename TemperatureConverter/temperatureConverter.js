505caa615df7a869bcdb54109cdffe1a1a1964e4
Anastasiya Kuznetcova <72085144+KuznetcovaAsya@users.noreply.github.com>
2021-07-10 16:53:59 +0700

    temperatureConverter

---
+++ TemperatureConverter/temperatureConverter.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var inputFieldText = document.getElementById("celsius-temperature");
+    var fahrenheitTemperature = document.getElementById("fahrenheit-temperature");
+    var kelvinTemperature = document.getElementById("kelvin-temperature");
+    var errorMessage = document.getElementById("error-message");
+
+    document.getElementById("button").addEventListener("click", function () {
+        var celsiusTemperature = inputFieldText.value;
+
+        if (isNaN(celsiusTemperature) || celsiusTemperature === "") {
+            errorMessage.textContent = "Введите число"
+            inputFieldText.value = "";
+            fahrenheitTemperature.textContent = "";
+            kelvinTemperature.textContent = "";
+            return;
+        }
+
+        errorMessage.textContent = "";
+
+        fahrenheitTemperature.textContent = parseFloat(celsiusTemperature) * 9 / 5 + 32;
+        kelvinTemperature.textContent = parseFloat(celsiusTemperature) + 273.15;
+    });
+});
