810ca52aa042b57e0049af99e81457cae4dec6eb
Anastasiya Kuznetcova <72085144+KuznetcovaAsya@users.noreply.github.com>
2021-08-02 09:18:03 +0700

    temperatureConverter

--- TemperatureConverter/temperatureConverter.js
+++ TemperatureConverter/temperatureConverter.js
@@ -2,19 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
     var inputFieldText = document.getElementById("celsius-temperature");
     var fahrenheitTemperature = document.getElementById("fahrenheit-temperature");
     var kelvinTemperature = document.getElementById("kelvin-temperature");
-    var errorMessage = document.getElementById("error-message");
+    var errorMessage = document.querySelector(".error-message");
 
-    document.getElementById("button").addEventListener("click", function () {
-        var celsiusTemperature = inputFieldText.value.trim();
+    document.querySelector(".button").addEventListener("click", function () {
+        var inputText = inputFieldText.value.trim();
 
-        if (celsiusTemperature.length === 0) {
+        if (inputText.length === 0) {
             errorMessage.textContent = "Введите число";
             fahrenheitTemperature.textContent = "";
             kelvinTemperature.textContent = "";
             return;
         }
 
-        celsiusTemperature = Number(inputFieldText.value.trim());
+        var celsiusTemperature = Number(inputText);
 
         if (isNaN(celsiusTemperature)) {
             errorMessage.textContent = "Введите число";
@@ -27,14 +27,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
         function getFahrenheitTemperature(celsiusTemperature) {
             return (celsiusTemperature * 9 / 5 + 32).toFixed(2);
-        };
+        }
 
         function getKelvinTemperature(celsiusTemperature) {
             return (celsiusTemperature + 273.15).toFixed(2);
-        };
+        }
 
         fahrenheitTemperature.textContent = getFahrenheitTemperature(celsiusTemperature);
-
         kelvinTemperature.textContent = getKelvinTemperature(celsiusTemperature);
     });
 });
