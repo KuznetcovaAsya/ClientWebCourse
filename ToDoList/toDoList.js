2c037de7457bbf287952644a597f09f3f26e782a
Anastasiya Kuznetcova <72085144+KuznetcovaAsya@users.noreply.github.com>
2021-08-11 12:45:57 +0700

    toDoList

--- ToDoList/toDoList.js
+++ ToDoList/toDoList.js
@@ -1,18 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
     var inputTextField = document.getElementById("input-text-field");
     var list = document.querySelector(".list");
-    var errorMessage = document.querySelector(".error-message");
 
     document.getElementById("add-button").addEventListener("click", function () {
         var text = inputTextField.value.trim();
         var savedTextBeforeCancel;
 
+        function clearAddErrorMessage() {
+            if (document.querySelector(".form").contains(document.querySelector(".add-error-message"))) {
+                document.querySelector(".add-error-message").remove();
+            }
+        }
+
+        function clearEditErrorMessage() {
+            if (document.querySelector(".list").contains(document.querySelector(".edit-error-message"))) {
+                document.querySelector(".edit-error-message").remove();
+            }
+        }
+
         if (text === "") {
-            errorMessage.textContent = "Please enter text";
+            if (!document.querySelector(".form").contains(document.querySelector(".add-error-message"))) {
+                var addErrorMessage = document.createElement("div");
+                addErrorMessage.className = "add-error-message";
+                addErrorMessage.textContent = "Please enter text";
+                list.before(addErrorMessage);
+            }
+
             return;
         }
 
-        errorMessage.textContent = "";
+        clearAddErrorMessage();
 
         function setViewMode() {
             listItem.innerHTML = "<span class='text'></span><button class='edit-button' type='button'>Edit</button><button class='delete-button' type='button'>Delete</button>"
@@ -20,6 +37,8 @@ document.addEventListener("DOMContentLoaded", function () {
             listItem.querySelector(".text").textContent = text;
 
             listItem.querySelector(".delete-button").addEventListener("click", function () {
+                clearAddErrorMessage();
+
                 listItem.parentNode.removeChild(listItem);
             });
 
@@ -28,19 +47,32 @@ document.addEventListener("DOMContentLoaded", function () {
                 listItem.querySelector(".edit-text").value = text;
                 savedTextBeforeCancel = text;
 
+                clearAddErrorMessage();
+
                 listItem.querySelector(".save-button").addEventListener("click", function () {
                     text = listItem.querySelector(".edit-text").value.trim();
 
                     if (text === "") {
-                        errorMessage.textContent = "Please enter text";
+                        if (!document.querySelector(".list").contains(document.querySelector(".edit-error-message"))) {
+                            var editErrorMessage = document.createElement("div");
+                            editErrorMessage.className = "edit-error-message";
+                            editErrorMessage.textContent = "Please enter text";
+                            listItem.after(editErrorMessage);
+                        }
+
                         return;
                     }
+
+                    clearAddErrorMessage();
+                    clearEditErrorMessage();
 
                     setViewMode();
                 });
 
                 listItem.querySelector(".cancel-button").addEventListener("click", function () {
-                    errorMessage.textContent = "";
+                    clearAddErrorMessage();
+                    clearEditErrorMessage();
+
                     text = savedTextBeforeCancel;
                     setViewMode();
                 });
